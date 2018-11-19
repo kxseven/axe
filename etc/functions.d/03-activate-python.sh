78c50645593e44186b337d2cf604b0e38180285b
Hugh Mooney <hugh.mooney@gmail.com>
2018-11-19 11:21:42 +0000

    Updates in advance of adding support for container usage

--- etc/functions.d/03-activate-python.sh
+++ etc/functions.d/03-activate-python.sh
@@ -3,6 +3,9 @@
 # TODO(kxseven: Better solution needed on OSX for forked shells from within Python
 if [[ "$(uname)" == "Darwin" ]]; then
    export PYTHONPATH="${AXE_ROOT}/local/python/lib/python2.7/site-packages:${AXE_ROOT}/lib:${AXE_ROOT}/bin/tools:${AXE_ROOT}/bin/subcommands"
+elif [[ "${AXE_CONTAINER}" == "docker" ]]; then
+       PYTHONPATH=$PYTHONPATH:${AXE_ROOT}/lib/python:${AXE_ROOT}/bin/tools:${AXE_ROOT}/bin/subcommands
+       export PYTHONPATH
 else
     if [ -e "${AXE_ROOT}/local/python/bin/activate" ]; then
        source ${AXE_ROOT}/local/python/bin/activate
